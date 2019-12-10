95b2208dcd00a12702848deed05d8755c4ef8e02
Allan Hall <bigal76126@gmail.com>
2019-12-09 22:53:45 -0600

    added dependencies and started home page

---
+++ ClientApp/src/pages/Home.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react'
+import Axios from 'axios'
+import { Link } from 'react-router-dom'
+
+export default function Home() {
+  const [searchTerm, setSearchTerm] = useState('')
+  const [feed, setFeed] = useState([])
+
+  useEffect(() => {
+    Axios.get(
+      `https://localhost:5001/api/feed`.then(resp => {
+        console.log({ resp })
+        setFeed(resp.data)
+      })
+    )
+  }, [])
+
+  const search = e => {
+    e.preventdefault()
+    Axios.get('/api/search?searchTerm=' + searchTerm).then(resp => {
+      setFeed(resp.data)
+    })
+  }
+
+  return (
+    <div>
+      <form onSubmit={search}>
+        <input
+          type="search"
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)}
+        />
+        <button>Search</button>
+      </form>
+      <ul className="feed">
+        {feed.map(index => {
+          return (
+            <li key={index}>
+              <Link>
+                <h2>{index.title}</h2>
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
